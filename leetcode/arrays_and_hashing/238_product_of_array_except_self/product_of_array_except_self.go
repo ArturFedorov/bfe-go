48270494a9b5eae7268520fffbde3f159787d8f8
Artur Fedorov <afedorov@gitlab.com>
2026-03-16 00:01:28 +0100

    Solve product of array except self, add two numbers, and merge two sorted lists

--- leetcode/arrays_and_hashing/238_product_of_array_except_self/product_of_array_except_self.go
+++ leetcode/arrays_and_hashing/238_product_of_array_except_self/product_of_array_except_self.go
@@ -1,5 +1,20 @@
 package product_of_array_except_self
 
 func productExceptSelf(nums []int) []int {
-	return nil
+	n := len(nums)
+	answer := make([]int, n)
+
+	leftProduct := 1
+	for i, num := range nums {
+		answer[i] = leftProduct
+		leftProduct *= num
+	}
+
+	rightProduct := 1
+	for i := n - 1; i >= 0; i-- {
+		answer[i] *= rightProduct
+		rightProduct *= nums[i]
+	}
+
+	return answer
 }
