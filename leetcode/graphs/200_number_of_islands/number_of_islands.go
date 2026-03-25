4223e7861592dc6bce2764ce0ca0b9ae64eec7ab
Artur Fedorov <afedorov@gitlab.com>
2026-03-26 00:21:56 +0100

    Solve LeetCode 200: Number of Islands and simplify LeetCode 21: Merge Two Sorted Lists

--- leetcode/graphs/200_number_of_islands/number_of_islands.go
+++ leetcode/graphs/200_number_of_islands/number_of_islands.go
@@ -1,5 +1,29 @@
 package number_of_islands
 
 func numIslands(grid [][]byte) int {
-	return 0
+	count := 0
+
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			if grid[i][j] == '1' {
+				count++
+			}
+
+			dfs(grid, i, j)
+		}
+	}
+
+	return count
+}
+
+func dfs(grid [][]byte, i, j int) {
+	if i < 0 || i >= len(grid) || j < 0 || j >= len(grid[i]) || grid[i][j] != '1' {
+		return
+	}
+
+	grid[i][j] = '2'
+	dfs(grid, i+1, j)
+	dfs(grid, i-1, j)
+	dfs(grid, i, j+1)
+	dfs(grid, i, j-1)
 }
--- leetcode/linked_lists/021_merge_two_sorted_lists/merge_two_sorted_lists.go
+++ leetcode/linked_lists/021_merge_two_sorted_lists/merge_two_sorted_lists.go
@@ -9,8 +9,8 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	dummy := &ListNode{}
 	curr := dummy
 
-	for list1 != nil && list2 != nil {
-		if list1.Val <= list2.Val {
+	for list1 != nil || list2 != nil {
+		if list1 != nil && (list2 == nil || list1.Val < list2.Val) {
 			curr.Next = list1
 			list1 = list1.Next
 		} else {
@@ -19,12 +19,6 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 		}
 
 		curr = curr.Next
-	}
-
-	if list1 != nil {
-		curr.Next = list1
-	} else {
-		curr.Next = list2
 	}
 
 	return dummy.Next
