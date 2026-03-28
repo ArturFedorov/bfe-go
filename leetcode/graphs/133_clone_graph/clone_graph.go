66d090e1f67cb783d4a1a28f4204929ee4e5698f
Artur Fedorov <afedorov@gitlab.com>
2026-03-28 23:09:48 +0100

    Solve LeetCode 133: Clone Graph

--- leetcode/graphs/133_clone_graph/clone_graph.go
+++ leetcode/graphs/133_clone_graph/clone_graph.go
@@ -6,5 +6,27 @@ type Node struct {
 }
 
 func cloneGraph(node *Node) *Node {
-	return nil
+	if node == nil {
+		return nil
+	}
+
+	copies := make([]*Node, 101)
+	dfs(node, copies)
+
+	return copies[node.Val]
+}
+
+func dfs(node *Node, copies []*Node) {
+	newNode := new(Node)
+	newNode.Val = node.Val
+
+	copies[node.Val] = newNode
+
+	for _, neighbour := range node.Neighbors {
+		if copies[neighbour.Val] == nil {
+			dfs(neighbour, copies)
+		}
+
+		newNode.Neighbors = append(newNode.Neighbors, copies[neighbour.Val])
+	}
 }
